f58b4b8cd562bdac74b9cddf1d7a7f527ba73618
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-12 07:12:48 +0900

    コマンドとバインドを実装

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -10,3 +10,18 @@ local function SendNotify(msg,type)
         QBCore.Functions.Notify(msg, 'primary')
     end
 end
+
+-- qb-menu
+RegisterCommand('test_qbmenu', function()
+    TriggerEvent('null-template-context_menu:client:openQBMenu')
+    SendNotify('Opening QBMenu', 'primary')
+end, false)
+
+-- ox-menu
+RegisterCommand('test_oxmenu', function ()
+    TriggerEvent('null-template-context_menu:client:openOXMenu')
+    SendNotify('Opening OXMenu', 'primary')
+end, false)
+
+RegisterKeyMapping('test_qbmenu', 'Open QB Menu', 'keyboard', 'F5')
+RegisterKeyMapping('test_oxmenu', 'Open OX Menu', 'keyboard', 'F6')
