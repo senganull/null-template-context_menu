cd475e01ece16b0d8256f496ae9278a2b71bcaa7
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-13 00:01:43 +0900

    ネットイベントを実装

---
+++ client/qb_example.lua
@@ -0,0 +1,38 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+--イベントハンドラ　メインメニュー
+RegisterNetEvent('null-template-context_menu:client:openQBMenu', function()
+    local menuList = {
+        {
+            header = "QB-Menu Example",
+            isMenuHeader = true, --クリックできないヘッダー設定
+            icon = "fa-slid fa-code"
+        },
+        {
+            header = "Simple Notification",
+            txt = "Click to see a notification",
+            params = {
+                event = "null-template-context_menu:client:showNotify",
+                args = {
+                    message = "Hello from QB-Menu",
+                    type = "success"
+                }
+            }
+        },
+        {
+            header = "Sub Menu Example",
+            txt = "Go to sub-menu options >",
+            params = {
+                event = "learning-menus:client:openSubMenu",
+            }
+        },
+        {
+            header = "Close Menu",
+            txt = "",
+            params = {
+                event = "qb-menu:client:closeMenu"
+            }
+        }
+    }
+    exports['qb-menu']:openMenu(menuList)
+end)
